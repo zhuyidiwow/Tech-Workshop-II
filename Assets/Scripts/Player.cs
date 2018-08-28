1c71ea5b0f7fd430cb241c8fcfd2119cc375ff41
Yidi-Z <zhuyidiwow@gmail.com>
2018-08-28 10:19:26 -0400

    Input setup

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	void Update() {
+		if (Input.GetMouseButtonDown(1)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+            Physics.Raycast(ray, out hit, 30f);
+
+			if (hit.collider != null && hit.collider.CompareTag("Ground")) {
+				Debug.DrawLine(transform.position, hit.point, Color.green, 1f);
+			}
+		}
+	}
+}
